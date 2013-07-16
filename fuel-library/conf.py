dd76493d81e98c939279751b445cf49dd9798ced
Pavel Lechenko <plechenko@mirantis.com>
2013-07-16 20:53:03 +0400

    Added cloud_sptheme

--- fuel-library/conf.py
+++ fuel-library/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import cloud_sptheme as csp
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
@@ -98,21 +99,21 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = "mirantis"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = { "roottarget": "index" }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_templates"]
+html_theme_path = ["_templates", csp.get_theme_dir()]
 
 html_add_permalinks = None
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
