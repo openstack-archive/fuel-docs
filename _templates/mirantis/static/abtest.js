c940cf6eb5b823b73db2d0be19ab52516267e7de
evkonstantinov <evkonstantinov@mirantis.com>
2014-12-30 16:21:46 +0300

    Add AB testing code to docs.mirantis.com layout
    The marketing team needs to be able to do AB testing on
    docs.mirantis.com.
    
    This commit places an AB testing js code in the doc layout
    right after the </title> tag on all the pages at docs.mirantis.com.
    
    Closes-Bug: 1328069
    
    Change-Id: Id9c2fad02db7e77ddda8684ef3cfe21211feb270

---
+++ _templates/mirantis/static/abtest.js
@@ -0,0 +1,1 @@
+<!-- begin Convert Experiments code--><script type="text/javascript">var _conv_host = (("https:" == document.location.protocol) ? "https://d9jmv9u00p0mv.cloudfront.net" : "http://cdn-1.convertexperiments.com");document.write(unescape("%3Cscript src='" + _conv_host + "/js/10012224-10012014.js' type='text/javascript'%3E%3C/script%3E"));</script><!-- end Convert Experiments code -->
