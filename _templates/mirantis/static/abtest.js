07db7a06c6a974c0f330ab8bcf8fc9272ece3950
Jim Phillips <jim@phillips.io>
2015-04-27 09:37:03 -0400

    add back to top
    Change-Id: Ib47a64ab80223e4f1e63bcdf931fcf6d48c5b43b

--- _templates/mirantis/static/abtest.js
+++ _templates/mirantis/static/abtest.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
 	if(filename == 'index.html' || filename == '') {
 		$('ul.nav.navbar-nav li.dropdown').not('.globaltoc-container').hide();
 	}
+
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+	scroll_top_duration = 700,
+	//grab the "back to top" link
+	$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible');
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
 });
