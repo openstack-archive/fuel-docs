27677c92fe47b67d63b18f29d15e2dbf88e48ddd
Jim Phillips <jim@phillips.io>
2015-05-16 09:50:04 -0400

    add social sharing buttons
    Change-Id: Ifc247b39393cad708cc52faa8e846bd703fd4d0a

--- common_conf.py
+++ common_conf.py
@@ -197,6 +197,8 @@ html_favicon = '_static/mirantis_icon.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_extra_path = ['_templates/mirantis/static/index.html']
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 # html_last_updated_fmt = '%c, %Z'
